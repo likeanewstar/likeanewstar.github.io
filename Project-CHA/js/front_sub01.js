61c766eba227266c70874457578355473e371cc2
강새별 <kangsaebyeol@gmail.com>
2017-12-12 00:43:24 +0900

    nav 태그 추가

---
+++ Project-CHA/js/front_sub01.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+	//gnb
+
+	$('ul.gnb>li').hover(function(){
+	
+		var num = $(this).index()
+		
+		$(this).find('ul.menu').show()
+		$(this).parents('#header').stop().animate({'height':'206px'},100)
+	},function(){
+		
+		$(this).find('ul.menu').hide()
+		$(this).parents('#header').stop().animate({'height':'156px'},100)
+	})
+
+	$('ul.menu').hover(function(){
+
+		$(this).show()
+		$(this).parents('#header').stop().animate({'height':'206px'},100)
+	},function(){
+		
+		$(this).hide()
+		$(this).parents('#header').stop().animate({'height':'156px'},100)
+	})
+
+
+	//top
+	
+	$(window).scroll(function(){ 
+	
+		var nowScroll = $(window).scrollTop()
+		console.log(nowScroll)
+			//if(nowScroll < 6500 && nowScroll > 900){
+			if(nowScroll >= $('.video').position().top){
+				$('p.top').fadeIn()
+			} else { $('p.top').hide() }//if
+	}) //scroll
+
+	$('p.top a').click(function(){
+			
+			event.preventDefault()
+
+			var pos = $('#header').position().top
+			$('html, body').animate({scrollTop:pos})
+	})
+
+	
+	//video
+
+	$('.video p.play a').click(function(){
+		
+		event.preventDefault()
+
+		$('.video div.popupVid').fadeIn()
+		$('p.top').fadeOut()
+	})
+
+	$('.video p.close a').click(function(){
+		
+		event.preventDefault()
+
+		$('.video div.popupVid').fadeOut()
+		$('p.top').fadeIn()
+	})
+	
+	//footer
+
+	$('#footer ul.link>li').hover(function(){
+	
+		$(this).find('ul').fadeIn(100)
+	}, function(){
+		
+		$(this).find('ul').hide()
+	})
+
+})//ready
+
+
